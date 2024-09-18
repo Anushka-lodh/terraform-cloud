f5ce884fe97f8e0b680079c6ec59d60024d1e65f
Anushka-lodh <anushkalodh777@gmail.com>
2024-09-18 22:46:38 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region_name
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_instance" "myec2" {
+
+  ami           = "ami-066784287e358dad1"
+  count = 3
+  instance_type = "t2.micro"
+tags= {
+ Name = "server"
+}
+}
